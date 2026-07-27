06298667810ba9dee240e95493762f06ec4dd276
Deenathayalan0511 <thayalandeena0511@gmail.com>
2026-07-27 20:49:26 +0530

    db file updated

--- server/config/db.js
+++ server/config/db.js
@@ -4,22 +4,28 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-     port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 try {
-    const connection = await pool.getConnection();
-    console.log("✅ MySQL Connected Successfully");
-    connection.release();
+  const connection = await pool.getConnection();
+  console.log("✅ MySQL Connected Successfully");
+  connection.release();
 } catch (error) {
-    console.error("❌ Database Connection Failed:", error.message);
+  console.error("❌ Database Connection Failed:");
+console.error(error);
 }
 
 export default pool;
