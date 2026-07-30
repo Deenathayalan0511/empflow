f00ce8a129be1beb5a104290e2d558121d65a270
Deenathayalan0511 <thayalandeena0511@gmail.com>
2026-07-30 12:01:19 +0530

    agin add in the mail

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,15 +1,20 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4, // Force IPv4
   connectionTimeout: 30000,
   greetingTimeout: 30000,
   socketTimeout: 30000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
