843dafe6c5b2ec447731939d335ad6ce1afc7049
Deenathayalan0511 <thayalandeena0511@gmail.com>
2026-08-18 14:52:05 +0530

    mail file change

--- server/config/mail.js
+++ server/config/mail.js
@@ -7,11 +7,15 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
